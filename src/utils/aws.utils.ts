1052aba307d8a2ecba9d1fae0449d7baa2ffa5bf
Mc Kellum <mck311um@gmail.com>
2025-02-21 19:25:41 -0400

    feat(AWS Utils): Load environment variables for AWS configuration

--- src/utils/aws.utils.ts
+++ src/utils/aws.utils.ts
@@ -3,7 +3,10 @@ import {
   PutObjectCommand,
   GetObjectCommand,
 } from "@aws-sdk/client-s3";
+import * as dotenv from "dotenv";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+dotenv.config();
 
 const awsBucketName = process.env.AWS_BUCKET_NAME;
 const awsRegion = process.env.AWS_REGION;
