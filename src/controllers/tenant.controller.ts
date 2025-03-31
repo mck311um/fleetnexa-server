031808a24e6fae230b6a6f9331a829c485a4f212
Mc Kellum <mck311um@gmail.com>
2025-03-30 23:35:32 -0400

    Add Customer and CustomerAddress models, update VehicleDamage schema, and modify tenant controller to include customers

--- src/controllers/tenant.controller.ts
+++ src/controllers/tenant.controller.ts
@@ -14,6 +14,7 @@ const getTenantById = async (req: Request, res: Response) => {
         currency: true,
         invoiceSequence: true,
         paymentMethods: true,
+        customers: true,
         vehicleGroups: {
           include: {
             discounts: true,
