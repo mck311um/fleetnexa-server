60774834c9b442b1acd190f9710a5e43d2a5cd21
Mc Kellum <mck311um@gmail.com>
2025-05-02 20:01:39 -0400

    Refactor code structure for improved readability and maintainability

--- prisma/schema/migrations/20250423224654_20250423184652/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "VehicleServiceSchedule" ADD COLUMN     "isManual" BOOLEAN NOT NULL DEFAULT false;
--- prisma/schema/migrations/20250425164158_20250425124156/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterEnum
-ALTER TYPE "StatItem" ADD VALUE 'AVAILABLE_VEHICLES';
