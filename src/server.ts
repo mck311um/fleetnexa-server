93351fd1da17e73046d1d37940c2f0240ef5cc53
Mc Kellum <mck311um@gmail.com>
2025-06-30 00:35:01 -0400

    feat: implement HTTPS support for production environment; enhance server setup and logging

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,33 @@ import app from "./app";
 import logUtil from "./config/logger.config";
 import { Server } from "socket.io";
 import http from "http";
+import https from "https";
+import fs from "fs";
+import path from "path";
 import setupSocket from "./config/socket";
 
 const PORT = process.env.PORT || 5001;
+const IS_PROD = process.env.NODE_ENV === "production";
 
-const server = http.createServer(app);
+let server: http.Server | https.Server;
+
+if (IS_PROD) {
+  const credentials = {
+    key: fs.readFileSync(
+      path.resolve("/etc/letsencrypt/live/api.fleetnexa.com/privkey.pem")
+    ),
+    cert: fs.readFileSync(
+      path.resolve("/etc/letsencrypt/live/api.fleetnexa.com/fullchain.pem")
+    ),
+  };
+
+  server = https.createServer(credentials, app);
+  logUtil.logger.info("✅ Using HTTPS server (production)");
+} else {
+  server = http.createServer(app);
+  logUtil.logger.info("🛠 Using HTTP server (development)");
+}
+
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -18,5 +40,5 @@ app.set("io", io);
 setupSocket(io);
 
 server.listen(PORT, () => {
-  logUtil.logger.info(`Server is running on port ${PORT}`);
+  logUtil.logger.info(`🚀 Server running on port ${PORT}`);
 });
