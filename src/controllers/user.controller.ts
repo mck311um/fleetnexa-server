ac2dd6e7e65f95b3d145562491e6ab01768f5e83
Mc Kellum <mck311um@gmail.com>
2025-07-12 20:31:45 -0400

    feat: add profilePicture field to User model; implement updateUser and updatePassword functionalities in user controller; adjust user routes

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,6 +3,7 @@ import prisma from "../config/prisma.config";
 import logUtil from "../config/logger.config";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { profile } from "console";
 
 const getCurrentUser = async (req: Request, res: Response) => {
   try {
@@ -19,6 +20,8 @@ const getCurrentUser = async (req: Request, res: Response) => {
         theme: true,
         color: true,
         roleId: true,
+        profilePicture: true,
+        requiredPasswordChange: true,
         role: {
           include: {
             rolePermission: {
@@ -54,6 +57,9 @@ const getCurrentUser = async (req: Request, res: Response) => {
       createdAt: user.createdAt,
       theme: user.theme,
       color: user.color,
+      email: user.email,
+      profilePicture: user.profilePicture || null,
+      requiredPasswordChange: user.requiredPasswordChange,
     };
 
     res.json(userData);
@@ -98,6 +104,172 @@ const getTenantUsers = async (
     next(error);
   }
 };
+const updateUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { user } = req.body;
+  const userId = req.user?.id;
+  const tenantId = req.user?.tenantId;
+
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { id: userId, tenantId },
+      data: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        profilePicture: user.profilePicture,
+      },
+      select: {
+        id: true,
+        username: true,
+        firstName: true,
+        lastName: true,
+        tenantId: true,
+        createdAt: true,
+        email: true,
+        theme: true,
+        color: true,
+        roleId: true,
+        profilePicture: true,
+        requiredPasswordChange: true,
+        role: {
+          include: {
+            rolePermission: {
+              include: {
+                permission: true,
+              },
+            },
+          },
+        },
+        tenant: {
+          select: {
+            tenantCode: true,
+            tenantName: true,
+          },
+        },
+      },
+    });
+
+    const userData = {
+      id: updatedUser.id,
+      username: updatedUser.username,
+      firstName: updatedUser.firstName,
+      lastName: updatedUser.lastName,
+      initials: `${updatedUser.firstName[0]}${updatedUser.lastName[0]}`,
+      fullName: `${updatedUser.firstName} ${updatedUser.lastName}`,
+      tenantId: updatedUser.tenantId,
+      tenant: updatedUser.tenant?.tenantCode,
+      tenantName: updatedUser.tenant?.tenantName,
+      createdAt: updatedUser.createdAt,
+      theme: updatedUser.theme,
+      color: updatedUser.color,
+      email: updatedUser.email,
+      profilePicture: updatedUser.profilePicture || null,
+      roleId: updatedUser.roleId,
+      role: updatedUser.role,
+      requiredPasswordChange: updatedUser.requiredPasswordChange,
+    };
+
+    res.status(201).json(userData);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updatePassword = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { currentPassword, newPassword } = req.body;
+  const userId = req.user?.id;
+
+  try {
+    await prisma.$transaction(async (prisma) => {
+      const user = await prisma.user.findUnique({
+        where: { id: userId },
+      });
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      const isMatch = await bcrypt.compare(currentPassword, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ message: "Current password is incorrect" });
+      }
+
+      if (currentPassword === newPassword) {
+        return res.status(400).json({
+          message: "New password cannot be the same as current password",
+        });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+      const updatedUser = await prisma.user.update({
+        where: { id: userId },
+        data: { password: hashedPassword, requiredPasswordChange: false },
+        select: {
+          id: true,
+          username: true,
+          firstName: true,
+          lastName: true,
+          tenantId: true,
+          createdAt: true,
+          email: true,
+          theme: true,
+          color: true,
+          roleId: true,
+          profilePicture: true,
+          requiredPasswordChange: true,
+          role: {
+            include: {
+              rolePermission: {
+                include: {
+                  permission: true,
+                },
+              },
+            },
+          },
+          tenant: {
+            select: {
+              tenantCode: true,
+              tenantName: true,
+            },
+          },
+        },
+      });
+
+      const userData = {
+        id: updatedUser.id,
+        username: updatedUser.username,
+        firstName: updatedUser.firstName,
+        lastName: updatedUser.lastName,
+        initials: `${updatedUser.firstName[0]}${updatedUser.lastName[0]}`,
+        fullName: `${updatedUser.firstName} ${updatedUser.lastName}`,
+        tenantId: updatedUser.tenantId,
+        tenant: updatedUser.tenant?.tenantCode,
+        tenantName: updatedUser.tenant?.tenantName,
+        createdAt: updatedUser.createdAt,
+        theme: updatedUser.theme,
+        color: updatedUser.color,
+        email: updatedUser.email,
+        profilePicture: updatedUser.profilePicture || null,
+        roleId: updatedUser.roleId,
+        role: updatedUser.role,
+        requiredPasswordChange: updatedUser.requiredPasswordChange,
+      };
+
+      res.status(201).json(userData);
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 const createTenantUser = async (req: Request, res: Response) => {
   const { firstName, lastName, email, roleId } = req.body;
   const tenantId = req.user?.tenantId;
@@ -150,4 +322,10 @@ const createTenantUser = async (req: Request, res: Response) => {
   }
 };
 
-export default { getCurrentUser, getTenantUsers, createTenantUser };
+export default {
+  getCurrentUser,
+  getTenantUsers,
+  createTenantUser,
+  updateUser,
+  updatePassword,
+};
