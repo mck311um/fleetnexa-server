011d4397a1e5f570744e461aef7167765fd238ea
Mc Kellum <mck311um@gmail.com>
2025-09-09 16:53:01 -0400

    feat(cors): update allowedOrigins to include new development URLs and remove outdated ones

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,13 +1,13 @@
 export const allowedOrigins = [
+  'http://localhost:3000',
   'http://localhost:5173',
   'http://localhost:5174',
-  'http://localhost:3000',
   'https://app.fleetnexa.com',
+  'https://dev.app.fleetnexa.com',
+  'https://dev.rentnexa.com',
   'https://fleetnexa.com',
-  'https://fleetnexa.vercel.app/',
   'https://rentnexa.com',
-  'https://rentnexa.vercel.app/',
   'https://www.fleetnexa.com',
-  'https://www.rentnexa.com',
   'https://www.fleetnexa.devvize.com',
+  'https://www.rentnexa.com',
 ];
