1b3accafbc2dade511ef08d30ef6de4c46fe10d1
Mc Kellum <mck311um@gmail.com>
2025-09-26 17:20:50 -0400

    refactor(tenant): remove unused service, equipment, and insurance routes and related controller methods

--- src/routes/tenant.routes.ts
+++ src/routes/tenant.routes.ts
@@ -4,25 +4,15 @@ import { auth } from '../middleware/auth.middleware';
 
 const router = express.Router();
 
-router.get('/services', auth, controller.getServices);
-router.get('/equipment', auth, controller.getEquipment);
-router.get('/insurance', auth, controller.getInsurance);
 router.get('/activity', auth, controller.getTenantRentalActivity);
 router.get('/reminders', auth, controller.getTenantReminders);
 router.get('/roles', auth, controller.getTenantRoles);
 router.get('/notifications', auth, controller.getTenantNotifications);
 router.get('/roles/:id', auth, controller.getTenantRolesById);
 
-// router.post('/', controller.createTenant);
-router.post('/service', auth, controller.addService);
-router.post('/equipment', auth, controller.addEquipment);
-router.post('/insurance', auth, controller.addInsurance);
 router.post('/role', auth, controller.addTenantRole);
 router.post('/reminder', auth, controller.addTenantReminder);
 
-router.put('/service', auth, controller.updateService);
-router.put('/equipment', auth, controller.updateEquipment);
-router.put('/insurance', auth, controller.updateInsurance);
 router.put('/roles/permissions', auth, controller.assignPermissionsToRole);
 router.put('/role/:id', auth, controller.updateTenantRole);
 
@@ -34,9 +24,6 @@ router.patch(
 );
 router.patch('/notification/:id', auth, controller.markNotificationAsRead);
 
-router.delete('/service/:id', auth, controller.deleteService);
-router.delete('/equipment/:id', auth, controller.deleteEquipment);
-router.delete('/insurance/:id', auth, controller.deleteInsurance);
 router.delete('/role/:id', auth, controller.markNotificationAsRead);
 router.delete('/notification/:id', auth, controller.deleteNotification);
 
