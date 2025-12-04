b3c7bdba0fefeefe495fa7ce5b9a965c0559f459
Mc Kellum <mck311um@gmail.com>
2025-12-04 19:25:06 -0400

    feat(pdf): implement PdfModule and PdfService for PDF generation and management

---
+++ src/common/pdf/pdf.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { PdfService } from './pdf.service.js';
+import { AwsModule } from '../aws/aws.module.js';
+
+@Module({
+  imports: [AwsModule],
+  providers: [PdfService],
+  exports: [PdfService],
+})
+export class PdfModule {}
