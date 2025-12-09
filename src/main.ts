c6aee5742f2fc4b0b5a28882242dcb959a5b2847
Mc Kellum <mck311um@gmail.com>
2025-12-08 20:38:21 -0400

    chore(cors): update CORS origins to include additional domains

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,15 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'https://dev.rentnexa.com/',
+      'https://rentnexa.com/',
+      'https://dev.fleetnexa.com/',
+      'https://fleetnexa.com/',
+      'https://admin.rentnexa.com/',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
     allowedHeaders: [
