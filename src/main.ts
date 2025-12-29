7b22430097807925394b6230b7cae28f28e8ad86
Mc Kellum <mck311um@gmail.com>
2025-12-29 08:10:40 -0400

    feat: add Swagger and Throttler modules, enhance security with Helmet
    - Added @nestjs/swagger for API documentation and configured Swagger setup in main.ts.
    - Integrated @nestjs/throttler for rate limiting in app.module.ts.
    - Included helmet for enhanced security in main.ts.
    - Updated package.json to include new dependencies.

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,67 @@
-import { NestFactory } from "@nestjs/core";
-import { config } from "dotenv";
-import { AppModule } from "./app.module.js";
-import { ValidationPipe } from "@nestjs/common";
-import { WinstonModule } from "nest-winston";
-import { winstonConfig } from "./config/winston.config.js";
-import * as crypto from "crypto";
+/** biome-ignore-all lint/suspicious/noExplicitAny: <> */
+import { NestFactory } from '@nestjs/core';
+import { config } from 'dotenv';
+import { AppModule } from './app.module.js';
+import { ValidationPipe } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from './config/winston.config.js';
+import * as crypto from 'node:crypto';
+import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 config();
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, {
-		logger: WinstonModule.createLogger(winstonConfig),
-	});
-	app.setGlobalPrefix("api");
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger(winstonConfig),
+  });
+  app.setGlobalPrefix('api');
+  app.use(helmet());
 
-	app.useGlobalPipes(
-		new ValidationPipe({
-			whitelist: true,
-			forbidNonWhitelisted: false,
-			transform: true,
-		}),
-	);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      transform: true,
+    }),
+  );
 
-	if (!(globalThis as any).crypto) {
-		(globalThis as any).crypto = crypto;
-	}
+  if (!(globalThis as any).crypto) {
+    (globalThis as any).crypto = crypto;
+  }
+  const config = new DocumentBuilder()
+    .setTitle('FleetNexa API')
+    .setDescription('FleetNexa API documentation')
+    .setVersion('1.0')
+    .addTag('fleetnexa')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
-	app.enableCors({
-		origin: [
-			"http://localhost:3000",
-			"http://localhost:5173",
-			"https://dev.rentnexa.com",
-			"https://rentnexa.com",
-			"https://dev.fleetnexa.com",
-			"https://dev.app.fleetnexa.com",
-			"https://app.fleetnexa.com",
-			"https://fleetnexa.com",
-			"https://admin.rentnexa.com",
-		],
-		methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-		credentials: true,
-		allowedHeaders: [
-			"Content-Type",
-			"Authorization",
-			"x-auth-token",
-			"x-timestamp",
-			"x-api-key",
-			"x-signature",
-		],
-	});
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'https://dev.rentnexa.com',
+      'https://rentnexa.com',
+      'https://dev.fleetnexa.com',
+      'https://dev.app.fleetnexa.com',
+      'https://app.fleetnexa.com',
+      'https://fleetnexa.com',
+      'https://admin.rentnexa.com',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'x-auth-token',
+      'x-timestamp',
+      'x-api-key',
+      'x-signature',
+    ],
+  });
 
-	await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
